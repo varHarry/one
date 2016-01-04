55c7148f6423a389d18cf27703cc18489a2a8610
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-04 09:43:08 -0500

    move js off of the page

---
+++ bitcoin/js/btc.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    var bitcoinPrice = ("https://blockchain.info/q/24hrprice");
+    $.getJSON(bitcoinPrice, function(cost) {
+        console.info('1 bitcoin = $', cost);
+        $(".bitcoin-price").append("<span>" + cost + "</span>");
+        var slushStats = ("https://mining.bitcoin.cz/stats/json/947244-fb33810230dadb19c00f1e13068cd5d6");
+        $.getJSON(slushStats, function(btcstats) {
+            for (var key in btcstats) {
+                if (btcstats.hasOwnProperty(key)) {
+                    console.info(btcstats);
+                    console.info(btcstats.blocks);
+                }
+            }
+            var monthNames = ["January", "February", "March", "April", "May", "June",
+                "July", "August", "September", "October", "November", "December"
+            ];
+            var h = new Date();
+            var day = h.getDate();
+            var year = h.getFullYear();
+            var hours = h.getHours();
+            var minutes = h.getMinutes();
+            var seconds = h.getSeconds();
+            var milliSeconds = h.getMilliseconds();
+            $(".btc-day").append("<span>" + monthNames[h.getMonth()] + " " + day + " " + year + " @ " + hours + ":" + minutes + ":" + seconds + ":" + milliSeconds + " </span>");
+            console.info("Timestamp: " + monthNames[h.getMonth()] + " " + day + " " + year + " @ " + hours + ":" + minutes + ":" + seconds + ":" + milliSeconds);
+        });
+    });
+});
