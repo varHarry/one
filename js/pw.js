5fecf3306011fb50fdcae14ad46f5a9bd19a7037
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-11 12:06:53 -0500

    starting to build the program

--- js/pw.js
+++ js/pw.js
@@ -1,1 +1,8 @@
 console.info("password generator check");
+
+<script>
+function myPassword() {
+    var generator = document.getElementsByTagName("p")[0].getAttribute("data-id");
+    document.getElementById("#").innerHTML = x;
+}
+</script>
