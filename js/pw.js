14732f916b602609830d8df3538d303320295416
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-11 12:07:26 -0500

    remove unnecessary tags

--- js/pw.js
+++ js/pw.js
@@ -1,8 +1,4 @@
-console.info("password generator check");
-
-<script>
 function myPassword() {
     var generator = document.getElementsByTagName("p")[0].getAttribute("data-id");
     document.getElementById("#").innerHTML = x;
 }
-</script>
