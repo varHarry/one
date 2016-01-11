0c8cad8d54066a3da6b1122a53802597a476193f
HarryMcKinney <harrypmckinney@gmail.com>
2016-01-11 12:26:25 -0500

    dial in

--- js/pw.js
+++ js/pw.js
@@ -1,5 +1,5 @@
 function myPassword() {
-    var generator = document.getElementsByTagName("p")[0].getAttribute("data-id");
+    var generator = document.getElementsByTagName(".pw-english")[0].getAttribute("data-id");
     console.info("password type: ", generator);
     if (generator === "english") {
         $(".english-result").append("");
